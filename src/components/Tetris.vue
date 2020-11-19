472cb76f3465bae39979a7bad45335546ea81dac
Semishogun <nectoj@gmail.com>
2020-11-19 13:18:56 +0100

    Initialised Project

---
+++ src/components/Tetris.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="tetris">
+        <h1>Tetris</h1>
+    </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class Tetris extends Vue {
+
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
